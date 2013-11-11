34f0f42fb94c8795aec984aed069b02f2ffbb901
Timothy Slater <timothy.slater@bellevue.edu>
2013-11-10 23:59:16 -0600

    Began implementing NUnit Tests for better test coverag of the decoder

--- CBOR_Test/Main.cs
+++ CBOR_Test/Main.cs
@@ -3,39 +3,103 @@ using System.IO;
 using CBOR;
 using System.Web;
 using System.Numerics;
+using System.Collections.Generic;
+using System.Collections;
+using NUnit.Framework;
+
 namespace CBOR_Test
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
+	public class DummyClass {
+		public static void Main(String[] args)
 		{
-			Console.WriteLine ("Hello World!");
 
-			BigInteger bi = new BigInteger(2);
-			bi = BigInteger.Pow (bi,64);
-			byte[] biBytes = bi.ToByteArray();
-			bi = new BigInteger();
-			// [1,[_ 2, 3],[4,5]]
-//			byte[] data = new byte[] {0x83, 0x01, 0x9F, 0x02, 0x03, 0xFF, 0x82, 0x04, 0x05};
+		}
 
-
-			//[_ -100 ]
-			//byte[] data = new byte[] {0x9F,0x38, 0x63,0xFF};
-
-
-			//{"a": 1, "b": [2, true,false,{"a":`01,02,03,04`,"b":[1.5f]}]}  
-			byte[] data = new byte[] {0xd9,0xd9,0xf7,0xa2, 0x61, 0x61, 0x01, 0x61, 0x62, 0x84, 0x02,0xF5,0xF4,0xa2,0x61, 0x61,0x44,0x01,0x02,0x03,0x04,0x61, 0x62,0x81,0xf9,0x3e,0x00}; 
-
-			// 1.5f
-			//byte[] data = new byte[] {0xf9,0x3e,0x00};
-
-			//-18446744073709551617 (BigInt (tags))
-			//byte[] data = new byte[]{0xc3,0x49,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-
+	}
+    [TestFixture]
+    public class DecoderTest
+    {
+        [Test]
+        public void IndefiniteArray()
+        {
+            //[_ -100, -100 ]
+			byte[] data = new byte[] {0x9F,0x38, 0x63,0x38, 0x63,0xFF};
 			CBORDecoder decoder = new CBORDecoder(data);
 
 			var f = decoder.ReadItem();
 
+			Assert.IsInstanceOf(typeof(ArrayList),f);
+
+			ArrayList list = (ArrayList)f;
+
+			Assert.AreEqual(2,list.Count);
+
+			Assert.AreEqual(-100,list[0]);
+
+			Assert.AreEqual(list[0],list[1]);
+        }
+
+		[Test]
+		public void SmallArray ()
+		{
+			// [1,2]
+			byte[] data = new byte[] {0x82, 0x01, 0x02};
+			CBORDecoder decoder = new CBORDecoder(data);
+
+			var f = decoder.ReadItem();
+
+			Assert.IsInstanceOf<ArrayList>(f);
 		}
-	}
+
+		[Test]
+		public void SmallPositiveInteger ()
+		{
+			byte[] data = new byte[]{0x08};
+			CBORDecoder decoder = new CBORDecoder (data);
+
+			var f = decoder.ReadItem ();
+
+			Assert.AreEqual(8,f);
+		}
+
+		[Test]
+		public void SmallMap()
+		{
+			// {"a":1,"b":"c"}
+			byte[] data = new byte[] {0xa2, 0x61, 0x61, 0x01, 0x61, 0x62,0x61,0x63};
+			CBORDecoder decoder = new CBORDecoder(data);
+
+			var f = decoder.ReadItem();
+
+			Assert.IsInstanceOf<Dictionary<String,object>>(f);
+
+			Dictionary<String,object> map = (Dictionary<String,object>)f;
+
+			Assert.AreEqual(2,map.Keys.Count);
+
+			f = null;
+		}
+
+		[Test]
+		public void NestedArray ()
+		{
+			// [[1],[2,[3,4]]]
+			byte[] data = new byte[] {0x82, 0x81, 0x01, 0x82, 0x02, 0x82, 0x03, 0x04};
+			CBORDecoder decoder = new CBORDecoder(data);
+
+			var f = decoder.ReadItem();
+			Assert.IsInstanceOf<ArrayList>(f);
+
+			ArrayList list1 = (ArrayList)f;
+			Assert.IsInstanceOf<ArrayList>(list1[0]);
+			Assert.IsInstanceOf<ArrayList>(list1[1]);
+
+			ArrayList list2 = (ArrayList)(list1[1]);
+
+			Assert.IsInstanceOf<ArrayList>(list2[1]);
+
+		}
+
+    }
+
 }
